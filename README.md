8f02a39390648cfa39b8b23ae50c1ef2ef5421ad
CloudyOverhead <jerome.simon@hotmail.com>
2020-08-27 16:25:05 -0400

    Commit module basics

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wavelet tessellation
+
+Inspired by June et al. (2015) Wavelet Tessellation and its Application to Downhole Gamma Data from the Manyingee & Bigrlyi Sandstone-Hosted Uranium Deposits.
---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright © Geolearn, Jerome Simon
+https://github.com/GeolearnAI/wavelet_tesselation
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="gwire",
+    version="0.0.1",
+    author="Geolearn",
+    # description="",
+    # long_description="",
+    # long_description_content_type="text/markdown",
+    url="https://github.com/GeolearnAI/gwire",
+    packages=find_packages(),
+    include_package_data=True,
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "Operating System :: OS Independent",
+    # ],
+    # python_requires='>=3.6',
+)
